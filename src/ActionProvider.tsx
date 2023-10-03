74788704ec0d2ab8afcd27b9ad7a483933faaf59
jenniferjfu2 <jennifer.fu@dominodatalab.com>
2023-10-02 22:05:34 -0700

    Commit changes for LangSmith

--- src/ActionProvider.tsx
+++ src/ActionProvider.tsx
@@ -6,21 +6,34 @@ import {
 } from 'langchain/agents';
 import { SerpAPI } from 'langchain/tools';
 import { Calculator } from 'langchain/tools/calculator';
+import { Client } from 'langsmith';
+import { LangChainTracer } from 'langchain/callbacks';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }: any) => {
   const executor = React.useRef<AgentExecutor>();
+  const tracer = React.useRef<LangChainTracer>();
 
   React.useEffect(() => {
     const init = async () => {
+      const client = new Client({
+        apiUrl: 'https://api.smith.langchain.com',
+        apiKey: import.meta.env.VITE_LANGCHAIN_API_KEY,
+      });
+
+      tracer.current = new LangChainTracer({
+        projectName: import.meta.env.VITE_LANGCHAIN_PROJECT,
+        client,
+      });
+
       // define a model
       const model = new ChatOpenAI({
         // use OPENAI_API_KEY
         openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY,
         // use GPT 4
         modelName: 'gpt-4',
-        // temperature is a number between 0 and 2, with a default value of 
-        // 1 or 0.7 depending on the selected model. The temperature is used 
-        // to control the randomness of the output. When it is set higher, 
+        // temperature is a number between 0 and 2, with a default value of
+        // 1 or 0.7 depending on the selected model. The temperature is used
+        // to control the randomness of the output. When it is set higher,
         // there will be more random outputs.
         temperature: 0,
       });
@@ -49,7 +62,10 @@ const ActionProvider = ({ createChatBotMessage, setState, children }: any) => {
   const generateResponse = async (message: string) => {
     if (executor.current) {
       // agent takes a list of messages as input and returns a message
-      const response = await executor.current.call({ input: message });
+      const response = await executor.current.call(
+        { input: message },
+        { callbacks: [tracer.current] }
+      );
       // the message is converted to a string message
       const botMessage = createChatBotMessage(response.output);
 
